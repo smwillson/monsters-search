9288c15fbd2928b40202996f3154d564195d25f4
Sama Mushtaq <sama.mushtaq@rgare.com>
2020-04-09 16:05:44 -0500

    Updates

---
+++ precache-manifest.45c0978620ecf930309090876625a9e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb5e95e1a6d5994f95cd044c559b96a",
+    "url": "/smwillson.github.io/monsters-search/index.html"
+  },
+  {
+    "revision": "022d6d4965835bdc5d21",
+    "url": "/smwillson.github.io/monsters-search/static/css/main.e366d042.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/smwillson.github.io/monsters-search/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smwillson.github.io/monsters-search/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022d6d4965835bdc5d21",
+    "url": "/smwillson.github.io/monsters-search/static/js/main.57fdbc19.chunk.js"
+  },
+  {
+    "revision": "0047ea75309d2f930502",
+    "url": "/smwillson.github.io/monsters-search/static/js/runtime-main.69f0240d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smwillson.github.io/monsters-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
